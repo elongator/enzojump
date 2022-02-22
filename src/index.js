835ce539b19e98c1a11240a8540b22640f25dbff
snatchstin <ashtin.evans@elongate.cc>
2022-02-22 09:32:20 -0800

    ty

--- src/index.js
+++ src/index.js
@@ -18,7 +18,7 @@ const config = {
     scale: {
         parent: 'game',
         autoCenter: Phaser.Scale.CENTER_BOTH,
-        mode: Phaser.Scale.FIT,
+        mode: Phaser.Scale.FIT_EXACT,
         width: 1000,
         height: 340
     },
